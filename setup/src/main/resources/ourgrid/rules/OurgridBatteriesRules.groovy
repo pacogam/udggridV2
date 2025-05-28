5b58adeb8fdcd711fc204956ab58ba8be2018fb7
Igor Rutka <igor.rutka@gmail.com>
2025-05-28 10:54:55 +0200

    Added OurGrid battery stop charging rule

--- setup/src/main/resources/ourgrid/rules/OurgridBatteriesRules.groovy
+++ setup/src/main/resources/ourgrid/rules/OurgridBatteriesRules.groovy
@@ -3,6 +3,7 @@ package ourgrid.rules
 import org.openremote.agent.custom.ourgrid.OurgridBatteryAsset
 import org.openremote.agent.custom.ourgrid.OurgridMeterAsset
 import org.openremote.manager.rules.RulesBuilder
+import org.openremote.manager.rules.RulesFacts
 import org.openremote.model.asset.Asset
 import org.openremote.model.attribute.AttributeInfo
 import org.openremote.model.query.AssetQuery
@@ -42,17 +43,6 @@ rules.add()
             return false
         })
         .then({ facts ->
-            // Check if parent asset ID is valid
-            def meterSumAsset = assets.getResults(new AssetQuery().ids(meterSumAssetId)).findFirst() as Optional<Asset>
-
-            if (meterSumAsset.isEmpty()) {
-                LOG.warning("No Meter Sum asset found with ID: '" + meterSumAssetId + "'; Check asset ID")
-                return
-            }
-
-            // Get household meter asset ID's
-            def meterAssetIds = assets.getResults(new AssetQuery().parents(meterSumAssetId).types(OurgridMeterAsset)).map { it.id }.toList() as String[]
-
             def attributeNames = [
                     OurgridBatteryAsset.ENERGY_LEVEL_PERCENTAGE.name,
                     OurgridBatteryAsset.ENERGY_LEVEL_PERCENTAGE_MAX.name,
@@ -60,22 +50,9 @@ rules.add()
                     OurgridBatteryAsset.POWER_SETPOINT.name
             ] as String[]
 
-            def batteriesAttributes = facts
-                    .matchAssetState(new AssetQuery().parents(meterAssetIds).types(OurgridBatteryAsset).attributeNames(attributeNames))
-                    .toList() as List<AttributeInfo>
+            def batteriesAttributes = getBatteriesAttributes(meterSumAssetId, attributeNames, facts) as Map<String, Map<String, Object>>
 
-            // Group attributes per asset ID
-            def batteriesAttributesGrouped = [:].withDefault { [:].withDefault { null } } as Map<String, Map<String, Object>>
-
-            batteriesAttributes.each { attributeInfo ->
-                def id = attributeInfo.id as String
-                def attributeName = attributeInfo.name as String
-                def value = attributeInfo.value.orElse(null)
-
-                batteriesAttributesGrouped[id][attributeName] = value
-            }
-
-            batteriesAttributesGrouped.each {
+            batteriesAttributes.each {
                 def assetId = it.key as String
                 def energyLevelPercentage = it.value.get(OurgridBatteryAsset.ENERGY_LEVEL_PERCENTAGE.name) as Double
                 def energyLevelPercentageMax = it.value.get(OurgridBatteryAsset.ENERGY_LEVEL_PERCENTAGE_MAX.name) as Double
@@ -95,3 +72,69 @@ rules.add()
                 }
             }
         })
+
+rules.add()
+        .priority(2)
+        .name("OurGrid battery stop charging rule")
+        .when({ facts ->
+            boolean triggerRule = false
+            long currentMillis = facts.clock.currentTimeMillis
+
+            // Trigger rule every 1 minute = 60000 ms
+            if (currentMillis > previousMillisRule2) {
+                previousMillisRule2 += 60000
+                triggerRule = true
+            }
+
+            return triggerRule
+        })
+        .then({ facts ->
+            def attributeNames = [
+                    OurgridBatteryAsset.ENERGY_LEVEL_PERCENTAGE.name,
+                    OurgridBatteryAsset.ENERGY_LEVEL_PERCENTAGE_MAX.name,
+                    OurgridBatteryAsset.POWER_SETPOINT.name
+            ] as String[]
+
+            def batteriesAttributes = getBatteriesAttributes(meterSumAssetId, attributeNames, facts) as Map<String, Map<String, Object>>
+
+            batteriesAttributes.each {
+                def assetId = it.key as String
+                def energyLevelPercentage = it.value.get(OurgridBatteryAsset.ENERGY_LEVEL_PERCENTAGE.name) as Double
+                def energyLevelPercentageMax = it.value.get(OurgridBatteryAsset.ENERGY_LEVEL_PERCENTAGE_MAX.name) as Double
+                def powerSetpoint = it.value.get(OurgridBatteryAsset.POWER_SETPOINT.name) as Double
+
+                if (powerSetpoint == null || energyLevelPercentage == null || energyLevelPercentageMax == null || (powerSetpoint > 0.0 && energyLevelPercentage >= energyLevelPercentageMax)) {
+                    assets.dispatch(assetId, OurgridBatteryAsset.POWER_SETPOINT.name, 0.0)
+                }
+            }
+        })
+
+private Map<String, Map<String, Object>> getBatteriesAttributes(String meterSumAssetId, String[] attributeNames, RulesFacts facts) {
+    // Check if parent asset ID is valid
+    def meterSumAsset = assets.getResults(new AssetQuery().ids(meterSumAssetId)).findFirst() as Optional<Asset>
+
+    if (meterSumAsset.isEmpty()) {
+        LOG.warning("No Meter Sum asset found with ID: '" + meterSumAssetId + "'; Check asset ID")
+        return
+    }
+
+    // Get household meter asset ID's
+    def meterAssetIds = assets.getResults(new AssetQuery().parents(meterSumAssetId).types(OurgridMeterAsset)).map { it.id }.toList() as String[]
+
+    def batteriesAttributesList = facts
+            .matchAssetState(new AssetQuery().parents(meterAssetIds).types(OurgridBatteryAsset).attributeNames(attributeNames))
+            .toList() as List<AttributeInfo>
+
+    // Group attributes per asset ID
+    def batteriesAttributes = [:].withDefault { [:].withDefault { null } } as Map<String, Map<String, Object>>
+
+    batteriesAttributesList.each { attributeInfo ->
+        def id = attributeInfo.id as String
+        def attributeName = attributeInfo.name as String
+        def value = attributeInfo.value.orElse(null)
+
+        batteriesAttributes[id][attributeName] = value
+    }
+
+    return batteriesAttributes
+}
